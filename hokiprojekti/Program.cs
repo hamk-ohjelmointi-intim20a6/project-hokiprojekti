2140428e222c9c20e27548195f3e052f0a4385a4
Hanna <Hanna.Sinkkila@student.hamk.fi>
2021-01-26 19:16:54 +0200

    Alustettu projekti, tehty .gitignore

---
+++ hokiprojekti/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hokiprojekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jee!");
+            Console.WriteLine("Paina mitä vaan sulkeaksesi konsolin");
+            Console.ReadKey();
+        }
+    }
+}
