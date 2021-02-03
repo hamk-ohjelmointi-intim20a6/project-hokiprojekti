7b5a42bfc608a04c50e59598fed6bb984679cc0b
Hanna <Hanna.Sinkkila@student.hamk.fi>
2021-02-03 17:23:09 +0200

    Päävalikon hahmottelua

--- hokiprojekti/Program.cs
+++ hokiprojekti/Program.cs
@@ -10,7 +10,23 @@ namespace hokiprojekti
     {
         static void Main(string[] args)
         {
-            
+            // Konsolin otsikko
+            Console.Title = "Änäri-äppi";
+
+            // Kysytään käyttäjältä aloitetaanko peli vai suljetaanko konsoli. 
+            // Antamalla "1" ohjelma jatkuu, mikä tahansa muu merkki sulkee konsolin.
+            Console.WriteLine("Tervetuloa Änäri-äppiin");
+            Console.WriteLine("1 Aloita uusi peli\n0 Sulje konsoli");
+            Console.WriteLine("Syötä 1/0");
+            string komento = Console.ReadLine();
+            if (komento.StartsWith("1"))
+            {
+                Console.Clear();
+                // tänne tulis sitten ohjelman jatkohommat
+                Console.WriteLine("Jee aloitetaan uusi peli!");
+                Console.WriteLine("Ohjelma sulkeutuu kun painat jotain nappia..");
+                Console.ReadKey();
+            }
         }
     }
 }
