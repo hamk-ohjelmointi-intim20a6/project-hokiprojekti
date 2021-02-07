687ff21edd67c951e2df4a8188dbb877002204f3
Hanna <Hanna.Sinkkila@student.hamk.fi>
2021-02-07 21:02:35 +0200

    Tehty päävalikolle oma luokka

--- hokiprojekti/Program.cs
+++ hokiprojekti/Program.cs
@@ -10,58 +10,19 @@ namespace hokiprojekti
     {
         static void Main(string[] args)
         {
+            Päävalikko päävalikko = new Päävalikko();
             // Konsolin otsikko
             Console.Title = "Änäri-äppi";
 
-            bool isRunning = true;
-
-            while (isRunning)
-            {
-                string komento = PyydäMerkkiä();
-
-                isRunning = AloitetaankoUusiPeliVaiLopetetaanko(komento);
-
-                if (komento != "1")
-                {
-                    continue;
-                }
-
-                AloitaUusiPeli();
-
-                Console.WriteLine("Paina mitä vaan nappia sulkeaksesi konsolin");
-                Console.ReadKey();
-                isRunning = false;
-            }
+            päävalikko.AlkuValikko();
+            AloitaUusiPeli();
         }
-        public static string PyydäMerkkiä()
-        {
-            Console.WriteLine("Tervetuloa Änäri-äppiin");
-            Console.WriteLine("1 Aloita uusi peli\n0 Sulje konsoli");
-            Console.WriteLine("Syötä 1/0");
-            string komento = Console.ReadLine();
-            return komento;
-        }
-        public static bool AloitetaankoUusiPeliVaiLopetetaanko(string komento)
-        {
-            if (komento == "1")
-            {
-                Console.Clear();
-                return true;
-            }
-            else if (komento == "0")
-            {
-                return false;
-            }
-            else
-            {
-                Console.Clear();
-                Console.WriteLine("Syötit virheellisen merkin, yritä uudestaan.");
-                return true;
-            }
-        }
+
         public static void AloitaUusiPeli()
         {
             Console.WriteLine("Aloitetaan uusi peli!");
+            Console.WriteLine("Paina mitä vaan nappia sulkeaksesi konsolin");
+            Console.ReadKey();
         }
     }
 }
---
+++ hokiprojekti/Päävalikko.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hokiprojekti
+{
+    class Päävalikko
+    {
+        public void AlkuValikko()
+        {
+            string komento = PyydäMerkkiä();
+            AloitetaankoUusiPeliVaiLopetetaanko(komento);
+
+            if (komento == "1")
+            {
+                return;
+            }
+            else
+            {
+                AlkuValikko();
+            }
+        }
+        public string PyydäMerkkiä()
+        {
+            Console.WriteLine("Tervetuloa Änäri-äppiin");
+            Console.WriteLine("1 Aloita uusi peli\n0 Sulje konsoli");
+            Console.WriteLine("Syötä 1/0");
+            string komento = Console.ReadLine();
+            return komento;
+        }
+        public void AloitetaankoUusiPeliVaiLopetetaanko(string komento)
+        {
+            if (komento == "1")
+            {
+                Console.Clear();
+            }
+            else if (komento == "0")
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Syötit virheellisen merkin, yritä uudestaan.\n");
+            }
+        }
+    }
+}
