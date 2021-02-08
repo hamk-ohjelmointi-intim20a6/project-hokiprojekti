b6d4cd9b1b12e33d1814bf369bbc06e769d65dfb
Noora Turunen <noorap.turunen@gmail.com>
2021-02-08 14:58:30 +0200

    Lisätty metodi valitsemaan koti- ja vieraspelit. Tulostaa otteluohjelman.

--- hokiprojekti/RoundRobin.cs
+++ hokiprojekti/RoundRobin.cs
@@ -18,25 +18,81 @@ namespace hokiprojekti
             while (n < rounds)
             {
 
-
+                //kierrätä listan elementit listassa yhden kierroksen verran, i = mones listaversio
                 for (var i = 1; i < players; i++)
                 {
-                    nimi = testilista[testilista.Count - 1];
-                    testilista.Remove(testilista[testilista.Count - 1]);
-                    testilista.Insert(1, nimi);
+                    nimi = testilista[testilista.Count - 1]; //luo uusi elementti listaan, joka on sama kuin listan viimeinen elementti
+                    testilista.Remove(testilista[testilista.Count - 1]); //poista listasta viimeinen elementti
+                    testilista.Insert(1, nimi); //lisää uusi elementti (pohja listan viimeinen elementti) listan kohtaan 2 eli indeksi 1
 
-                    foreach (string pelaaja in testilista)
-                    {
+                    ParillinenPariton(testilista, i); //metodi joka päättää koti- ja vieraspelin
+                    
 
-                        Console.WriteLine(pelaaja);
-                    }
-
-
-
-                    Console.ReadLine();
                 }
                 n++;
             }
+        }
+
+        //Metodi valitsee listaversion numerosta riippuen koti ja vieraspelin
+        public void ParillinenPariton(List<string> testilista, int i)
+        {
+            var x = i; //x = listaversio
+            if (x % 2 == 0) //jos listaversio on kahdella jaollinen, parillisilla listan indekseillä  kotiottelu
+            {
+                
+                ParillinenKoti(testilista, x);
+            }
+
+            if (x % 2 == 1) //jos listaversio on kahdella jaollinen, parittomilla listan indekseillä  kotiottelu
+            {
+                ParitonKoti(testilista, x);
+            }
+
+        }
+
+        //Listan parilliset indekseillä on kotiottelu
+        public void ParitonKoti(List<string> testilista, int x)
+        {
+            int ListanPituus; //listan pituudesta saadaan indeksit
+            ListanPituus = testilista.Count();
+            for (int i = 0; i < ListanPituus; i++)
+            {
+                if (i % 2 == 0) //jos indeksi parillinen -> kotiottelu, vastustaja on listan seuraava pariton indeksi
+                {
+                    Console.WriteLine("Kotiottelu: {0}" + " " + "Vieras: {1}", testilista[i], testilista[i+1]);
+                }
+
+                if (i % 2 == 1) // jos indeksi pariton jatka
+                { 
+                    continue;
+                }
+
+                
+            }
+
+            Console.ReadLine();
+        }
+
+        //Listan parittomilla indekseillä on kotiottelu
+        public void ParillinenKoti(List<string> testilista, int x)
+        {
+            int ListanPituus;
+            ListanPituus = testilista.Count();
+            for (int i = 0; i < ListanPituus; i++)
+            {
+                if (i % 2 == 0) //jos indeksi parillinen -> parillinen indeksi vieraspeli, vastustaja on listan seuraava pariton indeksi
+                {
+                    Console.WriteLine("Kotiottelu: {0}" + " " + "Vieras: {1}", testilista[i +1], testilista[i]);
+                }
+
+                if (i % 2 == 1)
+                {
+                    continue;
+                }
+
+            }
+
+            Console.ReadLine();
         }
     }
 }
