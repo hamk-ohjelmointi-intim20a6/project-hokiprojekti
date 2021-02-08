023f392234e02b0302723194f98745f17ade598d
Hanna <Hanna.Sinkkila@student.hamk.fi>
2021-02-08 12:29:19 +0200

    Hiottu koodia

--- hokiprojekti/Päävalikko.cs
+++ hokiprojekti/Päävalikko.cs
@@ -11,39 +11,34 @@ namespace hokiprojekti
         public void AlkuValikko()
         {
             string komento = PyydäMerkkiä();
-            AloitetaankoUusiPeliVaiLopetetaanko(komento);
-
-            if (komento == "1")
-            {
-                return;
-            }
-            else
-            {
-                AlkuValikko();
-            }
+            AloitetaankoUusiTurnausVaiLopetetaanko(komento);
         }
         public string PyydäMerkkiä()
         {
             Console.WriteLine("Tervetuloa Änäri-äppiin");
-            Console.WriteLine("1 Aloita uusi peli\n0 Sulje konsoli");
-            Console.WriteLine("Syötä 1/0");
+            Console.WriteLine("1 Aloita uusi turnaus\n0 Sulje konsoli");
             string komento = Console.ReadLine();
             return komento;
         }
-        public void AloitetaankoUusiPeliVaiLopetetaanko(string komento)
+        public void AloitetaankoUusiTurnausVaiLopetetaanko(string komento)
         {
-            if (komento == "1")
-            {
-                Console.Clear();
-            }
-            else if (komento == "0")
+            if (komento == "0")
             {
                 Environment.Exit(0);
             }
             else
             {
                 Console.Clear();
-                Console.WriteLine("Syötit virheellisen merkin, yritä uudestaan.\n");
+                if (komento == "1")
+                {
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Annoit virheellisen syötteen\n");
+                    AlkuValikko();
+                }
+
             }
         }
     }
