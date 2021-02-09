4d0063ca1c27e424bbce88407a99cc4c8671edb0
rikusjoroos <riku.sjoroos@student.hamk.fi>
2021-02-09 20:42:51 +0200

    Pääohjelmaa järjestelty.

--- hokiprojekti/Program.cs
+++ hokiprojekti/Program.cs
@@ -10,41 +10,29 @@ namespace hokiprojekti
     {
         static void Main(string[] args)
         {
-            //Testi commit
-            RoundRobin roundRobin = new RoundRobin();
-            List<string> testilista = new List<string>();
-            testilista.Add("1");
-            testilista.Add("2");
-            testilista.Add("3");
-            testilista.Add("4");
-            testilista.Add("5");
-            int rounds;
-            rounds = 4;
-            roundRobin.Robin(testilista, rounds);
-            /*List<Pelaaja> listapelaajista = otteluOhjelma1v1.Lista();
-            otteluOhjelma1v1.Printti(listapelaajista);*/
-
-
-            Console.ReadLine();
-
             // Konsolin otsikko
             Console.Title = "Änäri-äppi";
 
             Päävalikko.AlkuValikko();
-            AloitaUusiPeli();
-
-        }
-
-        public static void AloitaUusiPeli()
-        {
-            Console.WriteLine("Aloitetaan uusi peli!");
-            Console.WriteLine("Paina mitä vaan nappia sulkeaksesi konsolin");
-            Console.ReadKey();
 
             int pelaajienMäärä = Pelaajat.pelaajienMäärä();
-           
+
             List<string> listaPelaajista = Pelaajat.listaPelaajista(pelaajienMäärä);
+
+            Kierrokset kierrokset = new Kierrokset();
+            kierrokset.Kierrosluku();
+
+            Turnaustyyppi turnaustyyppi = new Turnaustyyppi();
+            turnaustyyppi.Turnausvalinta();
+
+            RoundRobin roundRobin = new RoundRobin();
             
+         
+            int rounds;
+            rounds = 4;
+            roundRobin.Robin(listaPelaajista, rounds);
+
+
         }
     }
 }
