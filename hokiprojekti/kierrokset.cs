001cb0f93de9cb41d624729ba3d6580652066943
Heidi <heidi.mattila@student.hamk.fi>
2021-02-09 12:35:00 +0200

    kierrosmäärän valinta

---
+++ hokiprojekti/kierrokset.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hokiprojekti
+{
+    public class Kierrokset
+    {
+        public void Kierrosluku()
+        {
+            Console.WriteLine("valitse pelattavien kierrosten määrä: ");
+            string syöte = Console.ReadLine();
+            Console.WriteLine("valitsit " + syöte + " kierrosta");
+
+           Console.ReadLine();
+            
+
+        }
+        
+        
+    }
+}
